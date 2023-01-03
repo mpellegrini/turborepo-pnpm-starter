c7a5aabfad530b3812cdb15e2a0fc6867b744b9c
Michael Pellegrini <466696+mpellegrini@users.noreply.github.com>
2023-01-02 22:49:39 -0500

    refactor(jest-config): remove jest support
    Comment out current tests until new test
    framework is available.

--- packages/example-pkg/src/subtract.test.ts
+++ packages/example-pkg/src/subtract.test.ts
@@ -1,15 +1,13 @@
-import { describe, expect, it } from '@jest/globals'
-
-import { subtract } from './subtract.js'
-
-describe('test subtract function', () => {
-  it('should return 5 for subtract(10,5)', () => {
-    expect.assertions(1)
-    expect(subtract(10, 5)).toBe(5)
-  })
-
-  it('should return -1 for subtract(2,3)', () => {
-    expect.assertions(1)
-    expect(subtract(2, 3)).toBe(-1)
-  })
-})
+// import { subtract } from './subtract.js'
+//
+// describe('test subtract function', () => {
+//   it('should return 5 for subtract(10,5)', () => {
+//     expect.assertions(1)
+//     expect(subtract(10, 5)).toBe(5)
+//   })
+//
+//   it('should return -1 for subtract(2,3)', () => {
+//     expect.assertions(1)
+//     expect(subtract(2, 3)).toBe(-1)
+//   })
+// })
